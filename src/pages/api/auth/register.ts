9fb1bbe8bad20cdd828302670953cbf1d3fd0cc5
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2025-07-14 00:39:12 +0200

    Add username for later integration in dashboard

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -4,16 +4,22 @@ import { supabase } from "../../../lib/supabase";
 
 export const POST: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData();
+  const display_name = formData.get("username")?.toString();
   const email = formData.get("email")?.toString();
   const password = formData.get("password")?.toString();
 
-  if (!email || !password) {
-    return new Response("Email and password are required", { status: 400 });
+  if (!email || !password || !display_name) {
+    return new Response("Email, password and username are required", { status: 400 });
   }
 
   const { error } = await supabase.auth.signUp({
     email,
     password,
+    options: {
+      data: {
+        display_name,
+      },
+    },
   });
 
   if (error) {
