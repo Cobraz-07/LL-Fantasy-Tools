ad252e4ebc47abc65b831eeee321345dc6d8da4b
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2025-07-15 01:06:37 +0200

    reset

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,10 +4,3 @@ export const supabase = createClient(
   import.meta.env.SUPABASE_URL,
   import.meta.env.SUPABASE_SERVICE_ROLE_KEY,
 );
-
-import { createBrowserClient } from "@supabase/ssr";
-
-export const supabaseSSR = createBrowserClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_SERVICE_ROLE_KEY,
-);
