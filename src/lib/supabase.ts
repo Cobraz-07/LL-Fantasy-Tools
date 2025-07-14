e5db24f29fd71422a0dcb2d200504fe2ef2a040e
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2025-07-14 18:29:40 +0200

    Initial implementation of database scripts and dashboard
    Will have to take the teams and be able to create teams, lets see.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,3 +4,10 @@ export const supabase = createClient(
   import.meta.env.SUPABASE_URL,
   import.meta.env.SUPABASE_SERVICE_ROLE_KEY,
 );
+
+import { createBrowserClient } from "@supabase/ssr";
+
+export const supabaseSSR = createBrowserClient(
+  import.meta.env.SUPABASE_URL,
+  import.meta.env.SUPABASE_SERVICE_ROLE_KEY,
+);
